4af99976801dfb1a1be85bb7fc5a607f234ad052
Александр Федоткин <ben115899@gmail.com>
2024-07-27 14:56:53 +0700

    Add constants

---
+++ WorldOfTasteApp/Constants/Colors.swift
@@ -0,0 +1,17 @@
+//
+//  Colors.swift
+//  WorldOfTasteApp
+//
+//  Created by Александр Федоткин on 24.07.2024.
+//
+
+import UIKit
+
+enum Colors {
+    static let whiteBackgroundColor: UIColor = #colorLiteral(red: 0.9529411765, green: 0.9529411765, blue: 0.9529411765, alpha: 1)
+    static let greenBackgroundColor: UIColor = #colorLiteral(red: 0.5764705882, green: 0.6823529412, blue: 0.5098039216, alpha: 1) //TODO: - Make gradient color
+    static let activeElementColor: UIColor = #colorLiteral(red: 0.1333333333, green: 0.1333333333, blue: 0.1333333333, alpha: 1)
+    static let activeElementTintColor: UIColor = #colorLiteral(red: 0.9725490196, green: 0.9725490196, blue: 0.9725490196, alpha: 1)
+    static let pageControlActiveColor: UIColor = #colorLiteral(red: 0.3529411765, green: 0.4470588235, blue: 0.2901960784, alpha: 1)
+    static let pageControlInactiveColor: UIColor = #colorLiteral(red: 0.737254902, green: 0.8862745098, blue: 0.6392156863, alpha: 1)
+}
