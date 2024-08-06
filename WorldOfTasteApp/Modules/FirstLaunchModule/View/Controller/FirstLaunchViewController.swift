bfd6e6dee2d4f9e0068251f1222eb9c6b3f95ca4
Александр Федоткин <ben115899@gmail.com>
2024-08-06 20:36:11 +0700

    setup data manager

--- WorldOfTasteApp/Modules/FirstLaunchModule/View/Controller/FirstLaunchViewController.swift
+++ WorldOfTasteApp/Modules/FirstLaunchModule/View/Controller/FirstLaunchViewController.swift
@@ -10,14 +10,21 @@ import UIKit
 final class FirstLaunchViewController: UIViewController {
     
     private let contentView = FirstLaunchView()
+    private let dataManager = FirstLaunchDataManager()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         setupUI()
+        
     }
     
     func setCurrentPage(number: Int) {
         contentView.setCurrentPage(number: number)
+        guard let data = dataManager.getData(with: number)
+        else {
+            return
+        }
+        contentView.setupData(data.page)
     }
 }
 
