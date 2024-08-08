cce34e7aa94aa132b5065ab8e54bc212b3861e71
Александр Федоткин <ben115899@gmail.com>
2024-08-08 23:32:23 +0700

    Add navigation menu

--- WorldOfTasteApp/Modules/MainScreenModule/HomeScreenModule/View/HomeScreenView.swift
+++ WorldOfTasteApp/Modules/MainScreenModule/HomeScreenModule/View/HomeScreenView.swift
@@ -9,8 +9,43 @@ import UIKit
 
 final class HomeScreenView: UIView {
     
-    override init(frame: CGRect) {
-        super.init(frame: frame)
+    private var navigationMenuTableView: UITableView = {
+        let tableView = UITableView()
+        tableView.backgroundColor = Colors.navigationMenuBackgroundColor
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.register(HomeTableViewCell.self, forCellReuseIdentifier: HomeTableViewCell.identifier)
+        tableView.separatorStyle = .none
+        return tableView
+    }()
+    
+    private var contentView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .lightGray
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
+    private lazy var navigationMenuButton: UIButton = {
+        let button = UIButton()
+        button.setImage(Images.MainScreenImages.HomeScreenImages.menuButton, for: .normal)
+        button.addTarget(self, action: #selector(rightSwipeAction), for: .touchUpInside)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
+    private var isMenuOpen: Bool = false
+    private let screenBounds = UIScreen.main.bounds
+    private var homeScale = CGAffineTransform()
+    private var tabBarController: UITabBarController?
+    
+    init(tableViewSettings: TableViewSettings, tabBarController: UITabBarController?) {
+        navigationMenuTableView.delegate = tableViewSettings
+        navigationMenuTableView.dataSource = tableViewSettings
+        self.tabBarController = tabBarController
+        super.init(frame: .zero)
+        setupUI()
+        setupMenuOpenAction()
+        setupMenuHidingAction()
     }
     
     @available(*, unavailable)
@@ -18,5 +53,99 @@ final class HomeScreenView: UIView {
         fatalError("init(coder:) has not been implemented")
     }
     
+}
+
+private extension HomeScreenView {
     
+    func setupUI() {
+        setupViewSettings()
+        addSubviews()
+        setupConstraints()
+        homeScale = contentView.transform
+    }
+    
+    func setupViewSettings() {
+        backgroundColor = Colors.navigationMenuBackgroundColor
+    }
+    
+    func addSubviews() {
+        addSubview(navigationMenuTableView)
+        addSubview(contentView)
+        contentView.addSubview(navigationMenuButton)
+    }
+    
+    func setupConstraints() {
+        NSLayoutConstraint.activate([
+            navigationMenuTableView.leftAnchor.constraint(equalTo: leftAnchor),
+            navigationMenuTableView.topAnchor.constraint(equalTo: topAnchor),
+            navigationMenuTableView.bottomAnchor.constraint(equalTo: bottomAnchor),
+            navigationMenuTableView.rightAnchor.constraint(equalTo: centerXAnchor, constant: 90)
+        ])
+        
+        NSLayoutConstraint.activate([
+            contentView.leftAnchor.constraint(equalTo: leftAnchor),
+            contentView.rightAnchor.constraint(equalTo: rightAnchor),
+            contentView.topAnchor.constraint(equalTo: topAnchor),
+            contentView.bottomAnchor.constraint(equalTo: bottomAnchor)
+        ])
+        
+        NSLayoutConstraint.activate([
+            navigationMenuButton.leftAnchor.constraint(equalTo: contentView.leftAnchor, constant: 27),
+            navigationMenuButton.topAnchor.constraint(equalTo: contentView.safeAreaLayoutGuide.topAnchor),
+            navigationMenuButton.heightAnchor.constraint(equalToConstant: 26),
+            navigationMenuButton.widthAnchor.constraint(equalToConstant: 26)
+        ])
+    }
+    
+    func setupMenuOpenAction() {
+        let gestureRecognizer = UISwipeGestureRecognizer()
+        gestureRecognizer.direction = .right
+        gestureRecognizer.addTarget(self, action: #selector(rightSwipeAction))
+        self.addGestureRecognizer(gestureRecognizer)
+    }
+    
+    func setupMenuHidingAction() {
+        let gestureRecognizer = UISwipeGestureRecognizer()
+        gestureRecognizer.direction = .left
+        gestureRecognizer.addTarget(self, action: #selector(leftSwipeAction))
+        self.addGestureRecognizer(gestureRecognizer)
+    }
+    
+    func showMenu() {
+        self.contentView.layer.cornerRadius = 40
+        self.layer.cornerRadius = self.contentView.layer.cornerRadius
+        let x = screenBounds.width * 0.8
+        let originalTransform = self.contentView.transform
+        let scaledTransform = originalTransform.scaledBy(x: 0.8, y: 0.8)
+        let scaledAndTranslatedTransform = scaledTransform.translatedBy(x: x, y: 0)
+        UIView.animate(withDuration: 0.7) {
+            self.contentView.transform = scaledAndTranslatedTransform
+            self.tabBarController?.tabBar.alpha = 0
+        } completion: { _ in
+            self.tabBarController?.tabBar.isHidden = true
+        }
+    }
+    
+    func hideMenu() {
+        UIView.animate(withDuration: 0.7) {
+            self.contentView.transform = self.homeScale
+            self.contentView.layer.cornerRadius = 0
+            self.tabBarController?.tabBar.isHidden = false
+            self.tabBarController?.tabBar.alpha = 1
+        }
+    }
+    
+    @objc func rightSwipeAction() {
+        if !isMenuOpen {
+            showMenu()
+            isMenuOpen = true
+        }
+    }
+    
+    @objc func leftSwipeAction() {
+        if isMenuOpen {
+            hideMenu()
+            isMenuOpen = false
+        }
+    }
 }
