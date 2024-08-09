df07415af3253ed5705c870e62364ccf8024595a
Vyacheslav <guzxaroh@gmail.com>
2024-08-09 15:06:28 +0700

    setup view for home controller

--- WorldOfTasteApp/Modules/MainScreenModule/HomeScreenModule/View/HomeScreenView.swift
+++ WorldOfTasteApp/Modules/MainScreenModule/HomeScreenModule/View/HomeScreenView.swift
@@ -9,14 +9,38 @@ import UIKit
 
 final class HomeScreenView: UIView {
     
+    private lazy var topWavyView: WavyTopView = {
+       let view = WavyTopView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.backgroundColor = .clear
+        return view
+    }()
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
+        setupView()
     }
     
     @available(*, unavailable)
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
+}
+
+private extension HomeScreenView {
     
+    func setupView() {
+        addSubview(topWavyView)
+        setupConstraints()
+    }
     
+    func setupConstraints() {
+        
+        NSLayoutConstraint.activate([
+            topWavyView.topAnchor.constraint(equalTo: self.topAnchor),
+            topWavyView.leadingAnchor.constraint(equalTo: self.leadingAnchor),
+            topWavyView.trailingAnchor.constraint(equalTo: self.trailingAnchor),
+            topWavyView.heightAnchor.constraint(equalToConstant: 270)
+        ])
+    }
 }
