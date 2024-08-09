7ce185eeaabb49f4455c410d4e5f2dc8c36c2264
Vyacheslav <guzxaroh@gmail.com>
2024-08-09 15:06:07 +0700

    added new font

--- WorldOfTasteApp/Constants/Fonts.swift
+++ WorldOfTasteApp/Constants/Fonts.swift
@@ -10,4 +10,5 @@ import UIKit
 enum Fonts {
     static let largeFont: UIFont? = UIFont(name: "PlusJakartaSans-Regular", size: 32)
     static let mediumFont: UIFont? = UIFont(name: "PlusJakartaSans-Regular", size: 17)
+    static let wavyTopViewSecondPartLabelFont: UIFont? = UIFont(name: "PlusJakartaSans-Regular", size: 54)
 }
