09edf531438da94bee9fe036fe59e2cc42d6bbe1
Vyacheslav <guzxaroh@gmail.com>
2024-08-09 15:06:41 +0700

    setup home controller

--- WorldOfTasteApp/Modules/MainScreenModule/HomeScreenModule/Controller/HomeViewController.swift
+++ WorldOfTasteApp/Modules/MainScreenModule/HomeScreenModule/Controller/HomeViewController.swift
@@ -9,8 +9,33 @@ import UIKit
 
 final class HomeViewController: UIViewController {
     
+    private lazy var homeScreenView: HomeScreenView = {
+        let view = HomeScreenView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupController()
+    }
+}
+
+private extension HomeViewController {
+    
+    func setupController() {
         view.backgroundColor = .white
+        
+        view.addSubview(homeScreenView)
+        setupConstraints()
+    }
+    
+    func setupConstraints() {
+        NSLayoutConstraint.activate([
+            homeScreenView.topAnchor.constraint(equalTo: view.topAnchor),
+            homeScreenView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            homeScreenView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            homeScreenView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
+        ])
     }
 }
